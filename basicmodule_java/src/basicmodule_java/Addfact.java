19488b86adffbcb4c0d280cffb4a4a01d937433d
USER <USER@FACE-35>
2019-09-20 19:10:17 +0530

    QUE1

--- basicmodule_java/src/basicmodule_java/Addfact.java
+++ basicmodule_java/src/basicmodule_java/Addfact.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Addfact {
-	public class Boxing {public static void main(String[] args)throws IOException
+	public class Boxing {public static void main (String[] args)throws IOException
 	{
 		BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
 		int num,fact,sum=0;
---
+++ basicmodule_java/src/basicmodule_java/Welcome.java
@@ -0,0 +1,13 @@
+package basicmodule_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Welcome {
+	public static void main(String[] args)throws IOException
+	{
+		BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+		string ip=br.
+	}
+}
